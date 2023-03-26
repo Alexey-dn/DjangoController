6783f58fddec46594bbd53739d00c9e004b4c727
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-03-26 13:44:43 +0300

    add verbose name

--- project/simpleapp/models.py
+++ project/simpleapp/models.py
@@ -7,29 +7,41 @@ class Product(models.Model):
     name = models.CharField(
         max_length=50,
         unique=True,  # названия товаров не должны повторяться
+        verbose_name='Название',
     )
-    description = models.TextField()
+    description = models.TextField(verbose_name='Описание')
     quantity = models.IntegerField(
         validators=[MinValueValidator(0)],
+        verbose_name='Количество',
     )
     # поле категории будет ссылаться на модель категории
     category = models.ForeignKey(
         to='Category',
         on_delete=models.CASCADE,
         related_name='products',  # все продукты в категории будут доступны через поле products
+        verbose_name='Категория'
     )
     price = models.FloatField(
         validators=[MinValueValidator(0.0)],
+        verbose_name='Цена',
     )
 
     def __str__(self):
-        return f'{self.name.title()}: {self.description[:20]}'
+        return f'{self.name}: {self.description[:20]}'
+
+    class Meta:  #  Возвращает название категории во множественном или единственном числе в админпанеле
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
 
 # Категория, к которой будет привязываться товар
 class Category(models.Model):
     # названия категорий тоже не должны повторяться
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name='Категория')
 
     def __str__(self):
-        return self.name.title()
+        return f'{self.name}'
+
+    class Meta:  #  Возвращает название категории во множественном или единственном числе в админпанеле
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
