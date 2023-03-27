1ddbf7952dae35091fbb570038c46abd1d6de260
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-03-27 21:21:54 +0300

    made path for product delete display

--- project/simpleapp/urls.py
+++ project/simpleapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 # Импортируем созданное нами представление
 from .views import (
-    ProductsList, ProductDetail, ProductCreate, ProductUpdate
+    ProductsList, ProductDetail, ProductCreate, ProductUpdate, ProductDelete
 )
 
 
@@ -18,4 +18,5 @@ urlpatterns = [
    # int — указывает на то, что принимаются только целочисленные значения
     path('create/', ProductCreate.as_view(), name='product_create'),
     path('<int:pk>/update/', ProductUpdate.as_view(), name='product_update'),
+    path('<int:pk>/delete/', ProductDelete.as_view(), name='product_delete'),
 ]
