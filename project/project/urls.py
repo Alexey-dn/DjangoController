f78131ba53be97a19234f50f8efd84186379daab
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-02 14:32:50 +0300

    add path for login

--- project/project/urls.py
+++ project/project/urls.py
@@ -21,6 +21,7 @@ Including another URLconf
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
    # Делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py)
    # подключались к главному приложению с префиксом products/.
