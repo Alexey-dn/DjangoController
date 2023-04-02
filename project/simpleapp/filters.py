6f2081ee97f664b0d290a205f60c43e604b5b8a8
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-02 13:55:28 +0300

    made search filter

--- project/simpleapp/filters.py
+++ project/simpleapp/filters.py
@@ -1,4 +1,4 @@
-from django_filters import FilterSet, ModelChoiceFilter
+from django_filters import FilterSet, ModelChoiceFilter, CharFilter, NumberFilter
 from .models import Product, Category
 
 
@@ -6,6 +6,12 @@ from .models import Product, Category
 # FilterSet, который мы наследуем,
 # должен чем-то напомнить знакомые вам Django дженерики.
 class ProductFilter(FilterSet):
+    name = CharFilter(
+        lookup_expr='icontains',
+        field_name='name',
+        label='Название товара',
+    )
+
     category = ModelChoiceFilter(
         field_name='category',
         queryset=Category.objects.all(),
@@ -13,19 +19,38 @@ class ProductFilter(FilterSet):
         empty_label='Любая',
     )
 
-    class Meta:
-        # В Meta классе мы должны указать Django модель,
-        # в которой будем фильтровать записи.
-        model = Product
-        # В fields мы описываем по каким полям модели
-        # будет производиться фильтрация.
-        fields = {
-            # поиск по названию
-            'name': ['icontains'],
-            # количество товаров должно быть больше или равно
-            'quantity': ['gt'],
-            'price': [
-                'lt',  # цена должна быть меньше или равна указанной
-                'gt',  # цена должна быть больше или равна указанной
-            ],
-        }
+    price = NumberFilter(
+        field_name='price',
+        label='Цена',
+    )
+
+    price__gt = NumberFilter(
+        field_name='price',
+        lookup_expr='gt',
+        label='Цена выше чем',
+    )
+
+    price__lt = NumberFilter(
+        field_name='price',
+        lookup_expr='lt',
+        label='Цена ниже чем',
+    )
+
+
+
+    # class Meta:
+    #     # В Meta классе мы должны указать Django модель,
+    #     # в которой будем фильтровать записи.
+    #     model = Product
+    #     # В fields мы описываем по каким полям модели
+    #     # будет производиться фильтрация.
+    #     fields = {
+    #         # поиск по названию
+    #         'name': ['icontains'],
+    #         # количество товаров должно быть больше или равно
+    #         'quantity': ['gt'],
+    #         'price': [
+    #             'lt',  # цена должна быть меньше или равна указанной
+    #             'gt',  # цена должна быть больше или равна указанной
+    #         ],
+    #     }
