f723daf747bc99bb2473f17c36f1a712c6ab7ced
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-02 15:08:46 +0300

    add app signup path into general urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -22,6 +22,7 @@ Including another URLconf
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("accounts.urls")),  # Важен порядок с предыдущим путем
     path('pages/', include('django.contrib.flatpages.urls')),
    # Делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py)
    # подключались к главному приложению с префиксом products/.
