b35ea47bf56fa4b1650f7b990b87b44d47b3e0ac
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-02 22:06:19 +0300

    add path for email login and commented path for username login

--- project/project/urls.py
+++ project/project/urls.py
@@ -21,9 +21,9 @@ Including another URLconf
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path("accounts/", include("accounts.urls")),  # Важен порядок с предыдущим путем
-    path('pages/', include('django.contrib.flatpages.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("allauth.urls")),  # Важен порядок с предыдущим путем
+    # path('pages/', include('django.contrib.flatpages.urls')),
    # Делаем так, чтобы все адреса из нашего приложения (simpleapp/urls.py)
    # подключались к главному приложению с префиксом products/.
     path('products/', include('simpleapp.urls')),
