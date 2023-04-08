3bdd1c11762f108c6039543f1f676c7a7fea1ed6
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-08 19:22:38 +0300

    add mail send adjustments

--- project/project/settings.py
+++ project/project/settings.py
@@ -155,3 +155,13 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_FORMS = {"signup": "accounts.forms.CustomSignupForm"}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "omneziya@yandex.ru"
+EMAIL_HOST_PASSWORD = "apwkwachtgcneyoo"  # rfuemvfwxcbzilqm
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = "omneziya@yandex.ru"
