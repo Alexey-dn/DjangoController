fa196ff19330bbc09e95ece539a404f6418ee9aa
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-20 18:57:20 +0300

    add mailsend task

--- project/simpleapp/tasks.py
+++ project/simpleapp/tasks.py
@@ -1,8 +1,24 @@
 from celery import shared_task
 import time
 
+from .views import Product
+from django.core.mail import mail_managers
+
 
 @shared_task
 def hello():
     time.sleep(10)
     print("Hello, world!")
+
+
+@shared_task
+def printer(N):
+    for i in range(N):
+        time.sleep(1)
+        print(i + 1)
+
+@shared_task
+def goods_notification():
+    products = Product.objects.order_by('price')[:3]
+    text = '\n'.join(['{} - {}'.format(p.name, p.price) for p in products])
+    mail_managers("Самые дешевые товары", text)
