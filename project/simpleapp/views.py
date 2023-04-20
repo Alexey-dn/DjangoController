1a2627d34fe782736c4db33064b6afd3e8700cb4
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-20 18:59:59 +0300

    add displaying tasks view

--- project/simpleapp/views.py
+++ project/simpleapp/views.py
@@ -25,7 +25,8 @@ from .models import Subscription, Category
 
 from django.http import HttpResponse
 from django.views import View
-from .tasks import hello
+from .tasks import hello, printer
+from datetime import datetime, timedelta
 
 
 class ProductsList(ListView):
@@ -188,5 +189,6 @@ def subscriptions(request):
 
 class IndexView(View):
     def get(self, request):
+        printer.apply_async([10], eta=datetime.now() + timedelta(seconds=5))
         hello.delay()
         return HttpResponse('Hello!')
