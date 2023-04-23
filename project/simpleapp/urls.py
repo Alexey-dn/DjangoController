f27f033385cbe3519b0c95d542362549f2d22c2a
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-23 14:58:15 +0300

    add caching for several pages

--- project/simpleapp/urls.py
+++ project/simpleapp/urls.py
@@ -6,16 +6,18 @@ from .views import (
 )
 from .views import IndexView
 
+# кэширование
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', ProductsList.as_view(), name='product_list'),
+    path('', cache_page(60)(ProductsList.as_view()), name='product_list'),
    # path — означает путь.
    # В данном случае путь ко всем товарам у нас останется пустым,
    # чуть позже станет ясно почему.
    # Т.к. наше объявленное представление является классом,
    # а Django ожидает функцию, нам надо представить этот класс в виде view.
    # Для этого вызываем метод as_view.
-    path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
+    path('<int:pk>', cache_page(60)(ProductDetail.as_view()), name='product_detail'),
    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
    # int — указывает на то, что принимаются только целочисленные значения
     path('create/', ProductCreate.as_view(), name='product_create'),
