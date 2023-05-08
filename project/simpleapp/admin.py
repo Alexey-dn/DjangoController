5d0ac408a10c5f0b88c2feec3f878067dfa1b90e
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-05-08 11:30:32 +0300

    add custom product fields view in admin panel

--- project/simpleapp/admin.py
+++ project/simpleapp/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+
 from .models import Category, Product
 
 
+# создаём новый класс для представления товаров в админке
+class ProductAdmin(admin.ModelAdmin):
+    # list_display = [field.name for field in Product._meta.get_fields()]
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    # генерируем список имён всех полей для более красивого отображения
+    list_display = ('name', 'price', 'on_stock')  # если нужны не все поля, оставляем только необходимые
+
+
 admin.site.register(Category)  # Не забываем зарегистрировать модели, иначе мы не увидим их в админке.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+# admin.site.unregister(Product) # для разрегистрации модели
