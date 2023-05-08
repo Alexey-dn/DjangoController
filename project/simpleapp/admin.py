fdfb2195122916655501d07c9903ec04974bb01a
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-05-08 12:00:06 +0300

    add filter and search fields

--- project/simpleapp/admin.py
+++ project/simpleapp/admin.py
@@ -3,12 +3,18 @@ from django.contrib import admin
 from .models import Category, Product
 
 
+
+
+
 # создаём новый класс для представления товаров в админке
 class ProductAdmin(admin.ModelAdmin):
     # list_display = [field.name for field in Product._meta.get_fields()]
     # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
     # генерируем список имён всех полей для более красивого отображения
     list_display = ('name', 'price', 'on_stock')  # если нужны не все поля, оставляем только необходимые
+    list_filter = ('price', 'quantity', 'name')  # добавляем примитивные фильтры в нашу админку
+    search_fields = ('name', 'category__name')  # тут всё очень похоже на фильтры из запросов в базу
+
 
 
 admin.site.register(Category)  # Не забываем зарегистрировать модели, иначе мы не увидим их в админке.
