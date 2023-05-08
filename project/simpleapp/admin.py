1f94c78aefc9aa5fa8f3687f4f98ac9db843e665
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-05-08 12:02:11 +0300

    add product zeroing action for admin panel

--- project/simpleapp/admin.py
+++ project/simpleapp/admin.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 from .models import Category, Product
 
 
+# напишем уже знакомую нам функцию обнуления товара на складе
+def nullfy_quantity(modeladmin, request, queryset):
+    # все аргументы уже должны быть вам знакомы, самые нужные из них это
+    # request — объект хранящий информацию о запросе и queryset — грубо говоря набор объектов, которых мы выделили галочками.
+    queryset.update(quantity=0)
 
+
+nullfy_quantity.short_description = 'Обнулить товары'  # описание для более понятного представления в админ панеле задаётся, как будто это объект
 
 
 # создаём новый класс для представления товаров в админке
@@ -14,7 +21,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'on_stock')  # если нужны не все поля, оставляем только необходимые
     list_filter = ('price', 'quantity', 'name')  # добавляем примитивные фильтры в нашу админку
     search_fields = ('name', 'category__name')  # тут всё очень похоже на фильтры из запросов в базу
-
+    actions = [nullfy_quantity]  # добавляем действия в список
 
 
 admin.site.register(Category)  # Не забываем зарегистрировать модели, иначе мы не увидим их в админке.
